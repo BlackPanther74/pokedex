246ee2a22545090d8f39d3a05309a665a58f1b77
Garth Puckerin <garth.puckerin@gmail.com>
2018-08-22 19:38:57 -0400

    Working on main.js

--- main.js
+++ main.js
@@ -210,42 +210,43 @@ axios.all([chooseRaichu, chooseLeafeon, chooseArceus])
 
         // });
         // for (let c = 0; c < catchem.length; c++) {
-        // function getMoves(poke1) {
-        //     makingMoves = poke1.moves;
-        //     let move = [];
-        //     let ctr = makingMoves.length;
-        //     for (let i = 0; i < 4; i++) {
-        //         let randMoves = Math.floor(Math.random() * ctr);
-        //         axios.get(makingMoves[randMoves].move.url)
-        //             .then(function (catchemBonus) {
-        //                 let pokemoves = catchemBonus.data;
-        //                 console.log(pokemoves);
-        //                 console.log(`Moves${i}: ${makingMoves[randMoves].move.name}
-        //                        Accruacy: ${pokemoves.accuracy}
-        //                        Power: ${pokemoves.power}
-        //                        Priority: ${pokemoves.priority}`);
+        function getMoves(poke1) {
+            makingMoves = poke1.moves;
+            let move = [];
+            let ctr = makingMoves.length;
+            for (let i = 0; i < 4; i++) {
+                let randMoves = Math.floor(Math.random() * ctr);
+                axios.get(makingMoves[randMoves].move.url)
+                    .then(function (catchemBonus) {
+                        let pokemoves = catchemBonus.data;
+                        console.log(pokemoves);
+                        console.log(`Moves${i}: ${makingMoves[randMoves].move.name}
+                               Accruacy: ${pokemoves.accuracy}
+                               Power: ${pokemoves.power}
+                               Priority: ${pokemoves.priority}`);
 
-        //                 move.push(`Moves${i}: \u00A0\u00A0${makingMoves[randMoves].move.name}\u00A0\u00A0
-        //                                      Accuracy: ${pokemoves.accuracy}\u00A0\u00A0
-        //                                      Power: ${pokemoves.power}\u00A0\u00A0
-        //                                      Priority: ${pokemoves.priority}`);
+                        move.push(`Moves${i}: \u00A0\u00A0${makingMoves[randMoves].move.name}\u00A0\u00A0
+                                             Accuracy: ${pokemoves.accuracy}\u00A0\u00A0
+                                             Power: ${pokemoves.power}\u00A0\u00A0
+                                             Priority: ${pokemoves.priority}`);
 
-        //                 // return 
-        //                 //     `Moves${i}: ${makingMoves[randMoves].move.name}
-        //                 //      Accruacy: ${bong.accuracy}
-        //                 //      Power: ${bong.power}
-        //                 //      Priority: ${bong.priority}`;
+                        // return 
+                        //     `Moves${i}: ${makingMoves[randMoves].move.name}
+                        //      Accruacy: ${bong.accuracy}
+                        //      Power: ${bong.power}
+                        //      Priority: ${bong.priority}`;
 
 
-        //             }).catch(function (response) {
-        //                 console.error(response);
-        //             })
-        //     }
-        //     return poke1.move = move;
-        //     // console.log(catchem[c].moves);
+                    }).catch(function (response) {
+                        console.error(response);
+                    })
+            }
+            return poke1.move = move;
+            // console.log(catchem[c].moves);
+        }
+
         // }
-
-        // }
+        getMoves(poke1);
 
         let raichuInfo = "It becomes aggressive when it has electricity stored up. At such times, even its Trainer has to take care to avoid being attacked."
 
@@ -325,4 +326,4 @@ axios.all([chooseRaichu, chooseLeafeon, chooseArceus])
         console.log(error);
     })
 
-console.log(raichu.get());
+console.log(naruto.get(leafeon));
